703239c3f542c9a7b39ffb576596129a094a01b6
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2021-12-10 20:06:13 -0300

    two_fer

---
+++ Src/two_fer.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Manoloon on 10/12/2021.
+//
+
+#include "two_fer.h"
+
+namespace two_fer
+{
+
+} // namespace two_fer
---
+++ Src/two_fer.h
@@ -0,0 +1,15 @@
+//
+// Created by Manoloon on 10/12/2021.
+//
+
+#ifndef EXERCISM_TWO_FER_H
+#define EXERCISM_TWO_FER_H
+
+
+namespace two_fer
+{
+
+} // namespace two_fer
+
+
+#endif //EXERCISM_TWO_FER_H
