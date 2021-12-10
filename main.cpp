f42d2936b1330612385c3ac7c4ccbf2064e7265e
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2021-12-10 12:34:58 -0300

    Pangram

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "Src/pangram.h"
 #include <string>
+#include <limits>
 
 int main()
 {
-   //std::string frase = "\"Five quacking Zephyrs jolt my wax bed.\"";
+   std::string frase = "\"Five quacking Zephyrs jolt my wax bed.\"";
    std::cout << "is pangram? " <<  pangram::is_pangram(frase) << std::endl;
+    std::cout << std::numeric_limits<std::byte>::min << std::endl;
    return 0;
 }
