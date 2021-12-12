730c25c35881e2b24a422260b0eb1b5245362ea7
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2021-12-12 15:04:26 -0300

    grade_school

---
+++ Src/grade_school.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Manoloon on 12/12/2021.
+//
+
+#include "grade_school.h"
+namespace grade_school
+{
+    school::school()
+    {
+
+    }
+}
---
+++ Src/grade_school.h
@@ -0,0 +1,19 @@
+//
+// Created by Manoloon on 12/12/2021.
+//
+
+#ifndef EXERCISM_GRADE_SCHOOL_H
+#define EXERCISM_GRADE_SCHOOL_H
+
+namespace grade_school
+{
+    class school
+    {
+    public:
+        school();
+    };
+
+}
+
+
+#endif //EXERCISM_GRADE_SCHOOL_H
