ff6338a0497cbb29ae4815ad388c1afd731f9555
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2022-03-04 10:31:37 +0100

    pangram.h

--- Src/pascal_triangle.cpp
+++ Src/pascal_triangle.cpp
@@ -22,15 +22,34 @@ namespace pascals_triangle
     }
     std::vector<std::vector<int>> generate_rows(int numRows)
     {
-        std::vector<std::vector<int>>Rows;
+        if(numRows == 0){return {{}};}
 
+        std::vector<std::vector<int>>Rows;
         for(int i=0;i<numRows;i++)
         {
             std::vector<int>row;
-            int number=i;
+            int number = i;
             for(int a=0;a<=number;a++)
             {
                 row.push_back(binomialCoefficient(number,a));
+            }
+            Rows.push_back({row});
+        }
+        return Rows;
+    }
+
+    std::vector<std::vector<int>> generate_rowsPerf(int numRows)
+    {
+        if(numRows == 0){return {{}};}
+
+        std::vector<std::vector<int>>Rows;
+        for(int i=0;i<numRows;i++)
+        {
+            int number = i;
+            std::vector<int>row;//(number,1);
+            for(int a=0;a<=number;a++)
+            {
+                row.push_back(a+a-1);
             }
             Rows.push_back({row});
         }
--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <string>
 #include "Src/pascal_triangle.h"
-#include "Src/VisualPatterns.h"
 
 int main()
 {
-    /**
     const std::vector<std::vector<int>> expected{{11}};
-    std::vector<std::vector<int>> actual = pascals_triangle::generate_rows(10);
+    std::vector<std::vector<int>> actual = pascals_triangle::generate_rowsPerf(3);
     std::cout << "result :" << (expected == actual) << std::endl;
     for (auto it = actual.begin(); it != actual.end(); ++it)
     {
@@ -16,10 +14,11 @@ int main()
                std::cout << element << " ";
            }
            std::cout << "\n";
-    }*/
+    }
+     /**
     VisualPatterns::HalfPyramidLeft(10);
     VisualPatterns::HalfInvertedPyramidLeft(10);
     VisualPatterns::FullPyramid(10);
-    VisualPatterns::Square(10);
+    VisualPatterns::Square(10);*/
    return 0;
 }
