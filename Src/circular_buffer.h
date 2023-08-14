8051378d007d834d14449cd11289bf3a417149f7
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2023-08-14 14:34:08 +0200

    circular_buffer

--- Src/circular_buffer.h
+++ Src/circular_buffer.h
@@ -1,52 +1,51 @@
 #if !defined(CIRCULAR_BUFFER_H)
 #define CIRCULAR_BUFFER_H
+
 #include <stdexcept>
 #include <deque>
 
 namespace circular_buffer
 {
-    template <typename T>
+    template<typename T>
     class circular_buffer
     {
     public:
-        explicit circular_buffer(int size):max_size(size)
+        explicit circular_buffer(size_t size) : max_size(size)
         {
-            if(max_size <=0){throw std::domain_error("number should be > 0");}
+            if (max_size < 1) { throw std::domain_error("number should be at least 1"); }
         }
 
-        const T& read()
+        T read()
         {
-            if(buffer.empty()){throw std::domain_error ("data not found");}
+            if (buffer.empty()) { throw std::domain_error("data not found"); }
 
-            T& value = buffer.front();
+            T value = buffer.front();
             buffer.pop_front();
             return value;
         }
 
-        void write(const T& value)
+        void write(const T &value)
         {
-            if (buffer.size() >= max_size){throw std::domain_error ("Buffer is full. Cannot push.");}
+            if (buffer.size() >= max_size) {
+                throw std::domain_error("Buffer is full. Cannot push.");
+            }
 
             buffer.push_back(value);
         }
 
-        void overwrite(const T& value)
+        void overwrite(const T &value)
         {
-            if(buffer.size() < max_size)
-            {
-                write(value);
+            if (buffer.size() == max_size) {
+                read();
             }
-            else
-            {
-                buffer.pop_front();
-                buffer.push_back(value);
-            }
+            write(value);
         }
 
         void clear()
         {
             buffer.clear();
         }
+
     private:
         size_t max_size;
         std::deque<T> buffer;
