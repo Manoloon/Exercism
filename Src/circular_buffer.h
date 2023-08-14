7d508b0bcee1bec719e61f41475df8f3728a9679
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2023-08-14 10:05:52 +0200

    circular_buffer

---
+++ Src/circular_buffer.h
@@ -0,0 +1,56 @@
+#if !defined(CIRCULAR_BUFFER_H)
+#define CIRCULAR_BUFFER_H
+#include <stdexcept>
+#include <deque>
+
+namespace circular_buffer
+{
+    template <typename T>
+    class circular_buffer
+    {
+    public:
+        explicit circular_buffer(int size):max_size(size)
+        {
+            if(max_size <=0){throw std::domain_error("number should be > 0");}
+        }
+
+        const T& read()
+        {
+            if(buffer.empty()){throw std::domain_error ("data not found");}
+
+            T& value = buffer.front();
+            buffer.pop_front();
+            return value;
+        }
+
+        void write(const T& value)
+        {
+            if (buffer.size() >= max_size){throw std::domain_error ("Buffer is full. Cannot push.");}
+
+            buffer.push_back(value);
+        }
+
+        void overwrite(const T& value)
+        {
+            if(buffer.size() < max_size)
+            {
+                write(value);
+            }
+            else
+            {
+                buffer.pop_front();
+                buffer.push_back(value);
+            }
+        }
+
+        [[maybe_unused]] void clear() const
+        {
+            buffer.clear();
+        }
+    private:
+        size_t max_size;
+        std::deque<T> buffer;
+    };
+}// namespace circular_buffer
+
+#endif // CIRCULAR_BUFFER_H
--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
+#include <memory>
 #include <string>
-#include "Src/pascal_triangle.h"
-#include "Src/VisualPatterns.h"
-#include "Src/Queen_Attack.h"
+#include "Src/circular_buffer.h"
 
 int main()
 {
-    const auto white = std::make_pair(4, 1);
-    const auto black = std::make_pair(2, 5);
-    const queen_attack::chess_board board{white, black};
-    std::cout << "result :" << (white == board.white()) << std::endl;
-    std::cout << "result :" << (black == board.black()) << std::endl;
+    circular_buffer::circular_buffer<int> buffer(2);
+    buffer.write(1);
+    buffer.overwrite(2);
+    std::cout << buffer.read();
+    std::cout << buffer.read();
+
+   circular_buffer::circular_buffer<std::string> bufferString(3);
+    bufferString.write("hello");
+    bufferString.write("world");
+    bufferString.write("zombies");
+   std::cout << bufferString.read();
+   bufferString.write("pants");
+   bufferString.overwrite("banana");
+   std::cout << bufferString.read();
+   std::cout << bufferString.read();
+   std::cout << bufferString.read();
    return 0;
 }
