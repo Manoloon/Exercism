3c9f44c9d5df8bf23e174611d7a44afd677db298
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2023-09-10 11:03:35 +0200

    Visual patterns

---
+++ Src/VisualPatterns.h
@@ -0,0 +1,18 @@
+//
+// Created by Manoloon on 22/02/2022.
+//
+
+#ifndef EXERCISM_VISUALPATTERNS_H
+#define EXERCISM_VISUALPATTERNS_H
+
+
+namespace VisualPatterns
+{
+    void HalfPyramidLeft(const int rows);
+    void HalfInvertedPyramidLeft(const int rows);
+    void FullPyramid(const int rows);
+    void Square(const int rows);
+}
+
+
+#endif //EXERCISM_VISUALPATTERNS_H
