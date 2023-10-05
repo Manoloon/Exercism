1091d23ba0f23f81b293d611797cbee83489df40
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2023-10-05 13:30:19 +0200

    grade_school.h

--- Src/grade_school.h
+++ Src/grade_school.h
@@ -1,25 +1,62 @@
 //
 // Created by Manoloon on 12/12/2021.
-//
+//school::add() takes its first argument by value but does not modify it.
+//F.16 of the C++ Core Guidelines says: "For “in” parameters, pass cheaply-copied types by value and others by reference to const"
+// In small examples like this you will not notice any difference, but if a copy is very expensive or if a function is called often avoiding the copy can be significantly more efficient.
 
+//add() handles two distinct cases: One where grade already exists in m_roster, one where it does not.
+//The code would become simpler if you'd only have to handle a single case. Here's something interesting about the std::map::operator[].
+// It inserts a new value-initialized element (in this case an empty std::vector) if there is no matching entry.
+// That modifying nature of std::map::operator[] is a surprise to many programmers coming from other languages.
+// That means you could store a reference to all_student[grade] in a local variable and then use that variable in the rest of the function.
+// That would shorten the code, make it simpler and IMHO easier to read.
 #ifndef EXERCISM_GRADE_SCHOOL_H
 #define EXERCISM_GRADE_SCHOOL_H
+
 #include <map>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 namespace grade_school
 {
     class school
     {
-        using Roster = std::map <int,std::vector<std::string>>;
-    private:
+        using Roster = std::map<int, std::vector<std::string>>;
         Roster m_roster;
+
     public:
- //       void add(std::vector<std::string>names,int grade);
-        void add(const std::string & name, const int grade);
-        const std::map <int,std::vector<std::string>> & roster() const;
-        const std::vector<std::string> & grade(int grade) const;
+        //       void add(std::vector<std::string>names,int grade);
+        void add(const std::string &name, const int grade)
+        {
+            // no need a copy of the map.
+            auto &allStudentOfGrade = m_roster[grade];
+            auto upperIt = std::ranges::upper_bound(allStudentOfGrade.begin(),
+                                                    allStudentOfGrade.end(),name);
+            allStudentOfGrade.insert(upperIt, name);
+        };
+
+        [[nodiscard]]
+        const Roster &roster() const
+        {
+            return m_roster;
+        };
+
+        [[nodiscard]]
+        const std::vector<std::string> &grade(int grade) const
+        {
+            auto const i = m_roster.find(grade);
+            static std::vector<std::string> emptyNameList;
+            if (i == m_roster.end())
+            {
+                return emptyNameList;
+            }
+            else
+            {
+                //But there's a more efficient alternative. Take a look at line 27 and let me know if you need another hint.
+                return i->second;
+            }
+        };
     };
 }
 #endif //EXERCISM_GRADE_SCHOOL_H
--- main.cpp
+++ main.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <memory>
 #include <string>
-#include "Src/circular_buffer.h"
+#include "Src/atbash_cipher.h"
 
 int main()
 {
-    circular_buffer::circular_buffer<int> buffer(2);
-    buffer.write(1);
-    buffer.overwrite(2);
-    std::cout << buffer.read();
-    std::cout << buffer.read();
+    std::cout <<  atbash_cipher::encode("Truth is fiction.") << std::endl;
+    std::cout <<  atbash_cipher::encode("Testing,1 2 3, testing.")<< std::endl;
+    std::cout <<  atbash_cipher::decode("vcvix rhn")<< std::endl;
+    std::cout <<  atbash_cipher::decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v")<< std::endl;
+    std::cout <<  atbash_cipher::decode("gvhgr mt123 gvhgr mt")<< std::endl;
+    // Dijkstra used
+    /*
+    int nodes = 6; // Number of nodes and edges
 
-   circular_buffer::circular_buffer<std::string> bufferString(3);
-    bufferString.write("hello");
-    bufferString.write("world");
-    bufferString.write("zombies");
-   std::cout << bufferString.read();
-   bufferString.write("pants");
-   bufferString.overwrite("banana");
-   std::cout << bufferString.read();
-   std::cout << bufferString.read();
-   std::cout << bufferString.read();
+    std::vector<std::vector<Edge>> graph(nodes);
+    std::vector<std::tuple<int,int,int>> Inputs = //Source Node , Destination node, edge weight
+            {
+                {0, 1, 7},
+                {0 ,2 ,9},
+                {0 ,5 ,14},
+                {1 ,2 ,10},
+                {1 ,3 ,15},
+                {2 ,3 ,11},
+                {2 ,5 ,2},
+                {3 ,4 ,6},
+                {4, 5, 9}
+            };
+
+    for (const auto& [sourceNode,DestinationNode,EdgeWeight] : Inputs)
+    {
+        graph[sourceNode].push_back({DestinationNode, EdgeWeight});
+        graph[DestinationNode].push_back({sourceNode, EdgeWeight}); // If your graph is undirected
+    }
+
+    int start = 0; // Starting node
+
+    std::vector<int> distance;
+    CalcPath(graph, start, distance);
+
+    // Print the shortest distances from the starting node to all other nodes
+    for (int i = 0; i < nodes; ++i) {
+        std::cout << "Shortest distance from node " << start << " to node " << i << " is " << distance[i] << std::endl;
+    }
+    ////////////////////////
+     */
    return 0;
 }
