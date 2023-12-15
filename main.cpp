3c2d728a2017de8668dfb094ae55e1820a21295e
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2023-12-15 09:26:51 +0100

    Three_sum

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <memory>
 #include <string>
+<<<<<<< HEAD
 #include "Src/atbash_cipher.h"
 #include "Src/CountBlocksOfOnes.h"
 
@@ -50,5 +51,34 @@ int main()
         std::cout << "Shortest distance from node " << start << " to node " << i << " is " << distance[i] << std::endl;
     }
      */
+=======
+#include <vector>
+#include "Src/circular_buffer.h"
+#include "Src/three_sum.h"
+
+int main()
+{
+    std::vector<int> nums ={-1,0,1,2,-1,-4};
+    for(const auto& [a, b, c] : three_sum(nums)) {
+        std::cout << a << ", " << b << ", " << c << std::endl;
+    }
+
+    circular_buffer::circular_buffer<int> buffer(2);
+    buffer.write(1);
+    buffer.overwrite(2);
+    std::cout << buffer.read();
+    std::cout << buffer.read();
+
+   circular_buffer::circular_buffer<std::string> bufferString(3);
+    bufferString.write("hello");
+    bufferString.write("world");
+    bufferString.write("zombies");
+   std::cout << bufferString.read();
+   bufferString.write("pants");
+   bufferString.overwrite("banana");
+   std::cout << bufferString.read();
+   std::cout << bufferString.read();
+   std::cout << bufferString.read();
+>>>>>>> d1043bca41e9bc8d18f4655b3fbbe8665ee8bb0a
    return 0;
 }
