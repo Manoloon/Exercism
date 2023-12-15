2dd805078b2adebbe70b59fc336e379eb06bceac
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2023-12-15 09:20:26 +0100

    data

--- Src/doctor_data.h
+++ Src/doctor_data.h
@@ -1,8 +1,55 @@
 //
 // Created by Manoloon on 15/12/2023.
 //
+#ifndef DOCTOR_DATA_H
+#define DOCTOR_DATA_H
+#pragma once
 
-#ifndef EXERCISM_DOCTOR_DATA_H
-#define EXERCISM_DOCTOR_DATA_H
+#include <iostream>
+#include <string>
 
-#endif //EXERCISM_DOCTOR_DATA_H
+namespace star_map {
+    enum class System {
+        BetaHydri,
+        EpsilonEridani,
+        Sol,
+        AlphaCentauri,
+        DeltaEridani,
+        Omicron2Eridani,
+        NONE
+    };
+}
+namespace heaven {
+    class Vessel {
+    public:
+        std::string name;
+        int generation;
+        star_map::System current_system = star_map::System::NONE;
+        int busters = 0;
+
+        Vessel(std::string _name, int _generation, star_map::System _system = star_map::System::NONE) :
+        name(_name),generation(_generation),current_system(_system) {}
+
+        Vessel replicate(std::string newName) { return Vessel(newName, this->generation + 1, star_map::System::Sol); }
+
+        bool shoot_buster() {
+            if (busters > 0) {
+                busters--;
+                return true;
+            }
+            return false;
+        }
+
+        void make_buster() { busters++; }
+    };
+
+    bool in_the_same_system(Vessel &A, Vessel &B) {
+        return (A.current_system == B.current_system);
+    }
+
+    std::string get_older_bob(Vessel &A, Vessel &B) {
+        return (A.generation < B.generation) ? A.name : B.name;
+    }
+
+}
+#endif //DOCTOR_DATA_H
--- main.cpp
+++ main.cpp
@@ -2,20 +2,26 @@
 #include <memory>
 #include <string>
 #include "Src/atbash_cipher.h"
+#include "Src/CountBlocksOfOnes.h"
 
 int main()
 {
+    std::cout << CountBlockOfOnes("101101110101111") << std::endl;
+    std::cout << CountBlockOfOnes("1") << std::endl;
+    std::cout << CountBlockOfOnes("00") << std::endl;
+    std::cout << CountBlockOfOnes("") << std::endl;
+    /*
     std::cout <<  atbash_cipher::encode("Truth is fiction.") << std::endl;
     std::cout <<  atbash_cipher::encode("Testing,1 2 3, testing.")<< std::endl;
     std::cout <<  atbash_cipher::decode("vcvix rhn")<< std::endl;
     std::cout <<  atbash_cipher::decode("zmlyh gzxov rhlug vmzhg vkkrm thglm v")<< std::endl;
     std::cout <<  atbash_cipher::decode("gvhgr mt123 gvhgr mt")<< std::endl;
     // Dijkstra used
-    /*
-    int nodes = 6; // Number of nodes and edges
+    int nodes = 6;
 
     std::vector<std::vector<Edge>> graph(nodes);
-    std::vector<std::tuple<int,int,int>> Inputs = //Source Node , Destination node, edge weight
+    //Source Node , Destination node, edge weight
+    std::vector<std::tuple<int,int,int>> Inputs =
             {
                 {0, 1, 7},
                 {0 ,2 ,9},
@@ -31,10 +37,10 @@ int main()
     for (const auto& [sourceNode,DestinationNode,EdgeWeight] : Inputs)
     {
         graph[sourceNode].push_back({DestinationNode, EdgeWeight});
-        graph[DestinationNode].push_back({sourceNode, EdgeWeight}); // If your graph is undirected
+        graph[DestinationNode].push_back({sourceNode, EdgeWeight});
     }
 
-    int start = 0; // Starting node
+    int start = 0;
 
     std::vector<int> distance;
     CalcPath(graph, start, distance);
@@ -43,7 +49,6 @@ int main()
     for (int i = 0; i < nodes; ++i) {
         std::cout << "Shortest distance from node " << start << " to node " << i << " is " << distance[i] << std::endl;
     }
-    ////////////////////////
      */
    return 0;
 }
