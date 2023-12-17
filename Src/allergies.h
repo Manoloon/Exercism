62c9d90155f2577eb323616f7929ab180cdc340d
Manoloon <pablo.r.sanchez@hotmail.com>
2023-12-17 08:35:11 +0100

    allergies

--- Src/allergies.h
+++ Src/allergies.h
@@ -1,6 +1,3 @@
-//
-// Created by pablo on 17/12/2023.
-//
 #if !defined(ALLERGIES_H)
 #define ALLERGIES_H
 #include <string>
@@ -9,26 +6,35 @@
 #include<utility>
 
 namespace allergies {
-    class allergy_test{
+class allergy_test{
     public:
-        explicit allergy_test(int _score):score(_score){}
-        bool is_allergic_to(const std::string& allergy){
-            const auto& it = allergiesMap.find(allergy);
-            return it != allergiesMap.end() && it->second <= score;
+    allergy_test(int _score):score(_score){
+    }
+
+    bool is_allergic_to(const std::string& allergy){
+        const auto& it = allergiesMap.find(allergy);
+        if(it != allergiesMap.end() && it->second <= score){
+            score -=it->second;
+            return true; 
         }
-        std::unordered_set<std::string> get_allergies() const {
-            std::unordered_set<std::string> allergiesNames;
-            for(const auto& it : allergiesMap){
-                allergiesNames.emplace(it.first);
-            }
-            return allergiesNames;
+          return false;     
+    }
+    std::unordered_set<std::string> get_allergies() const {
+        if(score == 0){
+            return {};
         }
+        std::unordered_set<std::string> allergiesNames;
+        for(const auto& it : allergiesMap){
+            allergiesNames.emplace(it.first);
+        }
+        return allergiesNames;
+    }
     private:
-        int score;
-        std::unordered_map<std::string,int> allergiesMap{
-                {"eggs",1},{"peanuts",2},{"shellfish",4},
-                {"strawberries",8},{"tomatoes",16},{"chocolate",32},
-                {"pollen",64},{"cats",128}};
+    int score;
+std::unordered_map<std::string,int> allergiesMap{
+    {"eggs",1},{"peanuts",2},{"shellfish",4},
+{"strawberries",8},{"tomatoes",16},{"chocolate",32},
+{"pollen",64},{"cats",128}};
     };
 }  // namespace allergies
 
