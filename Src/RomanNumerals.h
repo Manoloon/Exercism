9a21130e564cef38aa539284db0be0e28afc8c5c
Pablo Sanchez <14944505+Manoloon@users.noreply.github.com>
2024-01-02 08:11:30 +0100

    new ones

--- Src/RomanNumerals.h
+++ Src/RomanNumerals.h
@@ -22,12 +22,22 @@ In Roman numerals 1990 is MCMXC:
 1000=M 900=CM 90=XC
 2008 is written as MMVIII:
 2000=MM 8=VIII
+Review 2024 : update the limit to 4000 .
  */
 #ifndef EXERCISM_ROMANNUMERALS_H
 #define EXERCISM_ROMANNUMERALS_H
+#include <vector>
+#include <stdexcept>
 #include <string>
-namespace roman_numerals
-{
-    std::string convert(int n);
+
+namespace roman_numerals {
+    std::string convert(int n) {
+        if (n < 1 || n > 3999) throw std::invalid_argument("Number out of limits");
+        const std::vector<std::string> Unit{"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+        const std::vector<std::string> Tens{"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+        const std::vector<std::string> Hundred{"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+        const std::vector<std::string> Thousand{"", "M", "MM", "MMM"};
+        return Thousand[n / 1000] + Hundred[(n % 1000) / 100] + Tens[(n % 100) / 10] + Unit[n % 10];
+    }
 }
 #endif //EXERCISM_ROMANNUMERALS_H
